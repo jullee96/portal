d08ffc8efaf46d3004f7201495535631a9559a11
jullee <bono623@naver.com>
2022-03-04 17:02:08 +0900

    webconfig 경로 수정

--- src/main/java/com/hamonize/portal/WebConfig.java
+++ src/main/java/com/hamonize/portal/WebConfig.java
@@ -3,6 +3,7 @@ package com.hamonize.portal;
 import com.hamonize.portal.mail.IMailSender;
 import com.hamonize.portal.mail.MailSender;
 
+import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,14 +12,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Value("${attach.path}")
 	private String path;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        logger.info("WebConfig path >>> ? file:{}",path);
 	    registry.addResourceHandler("/img/**")
-        .addResourceLocations("file:"+path);
+        .addResourceLocations("file:"+path+"/");
         // .addResourceLocations("file:/home/lee/uploads/img/");
     }
 
